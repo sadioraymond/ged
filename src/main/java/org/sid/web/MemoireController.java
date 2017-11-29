b256c8ba6c8a53b32573e31ad4753ce51f42dafe
sadioraymond <sadiowalyraymond@gmail.com>
2017-11-29 14:13:24 +0000

    Ajout package Security

--- src/main/java/org/sid/web/MemoireController.java
+++ src/main/java/org/sid/web/MemoireController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemoireController {
-	@RequestMapping("/login")
-	public String index() {
-		return "login";
+	@RequestMapping("/acceuil")
+	public String acceuil() {
+		return "acceuil";
 	}
 
 }
